5d415c22379187ac325a610816acb61b1420a881
DesJC <jcd9930@hotmail.ca>
2018-05-12 15:36:29 -0400

    Testing Security

---
+++ XMLReader/SecurityLib/Security.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace SecurityLib
+{
+    /* Lots of this is from StackOverflow and .Net docs */
+    public static class Security
+    {
+        private static RNGCryptoServiceProvider _rand;
+        private static Random _unsecureRand;
+
+        static Security()
+        {
+            _rand = new RNGCryptoServiceProvider();
+            _unsecureRand = new Random();
+        }
+
+        public static string GeneratePassword()
+        {
+            string file = "worstPasswordList.txt";
+            try
+            {
+                using (StreamReader stream = new StreamReader(file))
+                {
+                    string[] passwords = stream.ReadToEnd().Split('\n');
+
+                    return passwords[_unsecureRand.Next(passwords.Length)];
+                }
+            }
+            catch (IOException)
+            {
+                throw new Exception("Error Accessing : " + file);
+            }
+
+        }
+
+        public static string Hash(string password, byte[] salt, int hashLength)
+        {
+            var hashedPassword = new Rfc2898DeriveBytes(password, salt, 10000)
+                .GetBytes(toBase64BitCount(hashLength) * 8);
+
+            return Convert.ToBase64String(hashedPassword);
+        }
+
+        public static string GetSalt(int length)
+        {
+            return Convert.ToBase64String(GetBytesSalt(toBase64BitCount(length)));
+        }
+
+        public static byte[] GetBytesSalt(int length)
+        {
+            byte[] salt = new byte[length];
+            _rand.GetBytes(salt);
+
+            return salt;
+        }
+
+        private static int toBase64BitCount(int bytesCount)
+        {
+            /* Each set of Base64 is formed of 6 bits */
+            return (int)Math.Ceiling((double)bytesCount / 6);
+        }
+    }
+}
--- XMLReader/XMLReader/Program.cs
+++ XMLReader/XMLReader/Program.cs
@@ -8,6 +8,7 @@ using System.IO;
 
 using System.Xml;
 using System.Xml.Linq;
+using SecurityLib;
 
 namespace XMLReader
 {
@@ -15,69 +16,77 @@ namespace XMLReader
     {
         static void Main(string[] args)
         {
-            /* Testing argument list */
-            if(args == null || args.Length <= 0)
-            {
-                throw new ArgumentException
-                    ("Invalid Argument:" + Environment.NewLine +
-                    "[NO ARGUMENT]" + Environment.NewLine +
-                    "Usage:" + Environment.NewLine +
-                    System.AppDomain.CurrentDomain.FriendlyName +
-                    " xmlFile0 [xmlFile1 xmlFile2 ... xmlFileN]"
-                    + Environment.NewLine);
-            }
+            /* Testing Security */
 
-            foreach(string xmlFile in args)
-            {
-                /* Testing File */
-                if(!File.Exists(xmlFile))
-                {
-                    throw new ArgumentException
-                        ("Invalid Argument:" + Environment.NewLine +
-                        xmlFile + Environment.NewLine +
-                        "Could not find file" + Environment.NewLine +
-                        "Please verify the file name is typed correctly"
-                        + Environment.NewLine);
-                }
+            string password = "MyPassword";
+            Console.WriteLine("Hashing : " + password);
+            byte[] salt = Security.GetBytesSalt(255);
+            Console.WriteLine("Salt : " + Convert.ToBase64String(salt));
+            Console.WriteLine("Hashed : " + Security.Hash(password, salt, 255));
 
-                try
-                {
-                    XDocument xmlDoc = XDocument.Load(xmlFile);
+            ///* Testing argument list */
+            //if(args == null || args.Length <= 0)
+            //{
+            //    throw new ArgumentException
+            //        ("Invalid Argument:" + Environment.NewLine +
+            //        "[NO ARGUMENT]" + Environment.NewLine +
+            //        "Usage:" + Environment.NewLine +
+            //        System.AppDomain.CurrentDomain.FriendlyName +
+            //        " xmlFile0 [xmlFile1 xmlFile2 ... xmlFileN]"
+            //        + Environment.NewLine);
+            //}
 
-                    string rootElement = xmlDoc.Root.Name.ToString();
-                    switch (rootElement)
-                    {
-                        case "questions":
-                            Console.WriteLine("Parsing Questions");
-                            using (db1633477Entities db = new db1633477Entities())
-                            {
-                                QuestionParserXML.parse(db, xmlDoc);
-                            }
-                            break;
-                        case "users":
-                            Console.WriteLine("Not Yet Parsing Users");
-                            //Parsing users
-                            break;
-                        default:
-                            Console.Error.WriteLine
-                                ("Invalid Argument:" + Environment.NewLine +
-                                rootElement + Environment.NewLine +
-                                "Has not associated parsing Class" + Environment.NewLine +
-                                "Ignoring" + Environment.NewLine);
-                            break;
-                    }
-                }
-                /* If the file is not a valid xml Document */
-                catch(XmlException)
-                {
-                    throw new ArgumentException
-                        ("Invalid Argument:" + Environment.NewLine +
-                        xmlFile + Environment.NewLine +
-                        "Could not read file" + Environment.NewLine +
-                        "Please verify the file is a well-formed and valid xml file"
-                        + Environment.NewLine);
-                }
-            }
+            //foreach(string xmlFile in args)
+            //{
+            //    /* Testing File */
+            //    if(!File.Exists(xmlFile))
+            //    {
+            //        throw new ArgumentException
+            //            ("Invalid Argument:" + Environment.NewLine +
+            //            xmlFile + Environment.NewLine +
+            //            "Could not find file" + Environment.NewLine +
+            //            "Please verify the file name is typed correctly"
+            //            + Environment.NewLine);
+            //    }
+
+            //    try
+            //    {
+            //        XDocument xmlDoc = XDocument.Load(xmlFile);
+
+            //        string rootElement = xmlDoc.Root.Name.ToString();
+            //        switch (rootElement)
+            //        {
+            //            case "questions":
+            //                Console.WriteLine("Parsing Questions");
+            //                using (db1633477Entities db = new db1633477Entities())
+            //                {
+            //                    QuestionParserXML.parse(db, xmlDoc);
+            //                }
+            //                break;
+            //            case "users":
+            //                Console.WriteLine("Not Yet Parsing Users");
+            //                //Parsing users
+            //                break;
+            //            default:
+            //                Console.Error.WriteLine
+            //                    ("Invalid Argument:" + Environment.NewLine +
+            //                    rootElement + Environment.NewLine +
+            //                    "Has not associated parsing Class" + Environment.NewLine +
+            //                    "Ignoring" + Environment.NewLine);
+            //                break;
+            //        }
+            //    }
+            //    /* If the file is not a valid xml Document */
+            //    catch(XmlException)
+            //    {
+            //        throw new ArgumentException
+            //            ("Invalid Argument:" + Environment.NewLine +
+            //            xmlFile + Environment.NewLine +
+            //            "Could not read file" + Environment.NewLine +
+            //            "Please verify the file is a well-formed and valid xml file"
+            //            + Environment.NewLine);
+            //    }
+            //}
         }
     }
 }

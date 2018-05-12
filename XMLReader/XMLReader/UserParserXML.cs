05a94b41f729d7e5a647785c8f7fafedb8e7e00e
DesJC <jcd9930@hotmail.ca>
2018-05-12 19:00:43 -0400

    Finish the xml parsing app

--- XMLReader/XMLReader/UserParserXML.cs
+++ XMLReader/XMLReader/UserParserXML.cs
@@ -1,12 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Xml;
+using System.Xml.Linq;
+
+using SecurityLib;
+
 namespace XMLReader
 {
-    class UserParserXML
+    public static class UserParserXML
     {
+        public static void Parse(db1633477Entities db, XDocument xmlDocument)
+        {
+            string uname;
+            string password;
+            string salt;
+            byte[] saltBytes;
+
+            foreach (XElement userXML in xmlDocument.Root.Elements())
+            {
+                password = XMLParserHelper.GetStringAttribute(userXML, "password") ?? "password";
+                salt = Security.GetSalt(255, out saltBytes);
+
+                uname = XMLParserHelper.GetStringAttribute(userXML, "userId");
+                Console.WriteLine("Parsing...\t" + uname);
+
+                db.USERs.Add(new USER
+                {
+                    UNAME = uname,
+                    PASSW_HASH = Security.Hash(password, saltBytes, 255),
+                    SALT = salt,
+                    FNAME = XMLParserHelper.GetStringAttribute(userXML, "firstName"),
+                    LNAME = XMLParserHelper.GetStringAttribute(userXML, "lastName")
+                });
+            }
+
+            Console.WriteLine("Saving Changes...");
+            try
+            {
+                db.SaveChanges();
+            }
+            catch(DbUpdateException exc)
+            {
+                Console.WriteLine(exc);
+                throw exc;
+
+            }
+            catch(DbEntityValidationException exc)
+            {
+                foreach(DbEntityValidationResult error in exc.EntityValidationErrors)
+                {
+                    Console.WriteLine(error.ToString() + " :\n" +
+                        error.Entry.Entity.ToString() + " is" + (error.IsValid ? " " : " not ") + "valid\n");
+                    foreach(DbValidationError problem in error.ValidationErrors)
+                    {
+                        Console.WriteLine(problem.ErrorMessage);
+                    }
+                }
+                throw exc;
+            }
+        }
     }
 }
